04beeac2a9820302d2288d3acfb30d6f944c7e08
MatteoCantafio25 <mattyteo004@gmail.com>
2024-01-16 10:54:39 +0100

    modified

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 - Recupero gli elementi dalla pagina
 - Creo l'array di oggetti
+- Creo una variabile di appoggio
+- Uso un ciclo per montare la card con elementi dinamici
 - Stampo in pagina le informazioni prese dall'array
-
--- script.js
+++ script.js
@@ -5,70 +5,38 @@ console.log("JS OK");
 
 // RECUPERO GLI ELEMENTI DALLA PAGINA
 
-const wayneElement = document.getElementById("wayne");
-const angelaElement = document.getElementById("angela");
-const walterElement = document.getElementById("walter");
-const lopezElement = document.getElementById("lopez");
-const scottElement = document.getElementById("scott");
-const barbaraElement = document.getElementById("barbara");
-
-
+const cardContainer = document.querySelector(".cols");
 
 // CREO L'ARRAY DI OGGETTI
 
 const team = [
-    { name: "Wayne Barnett", role: "Founder & CEO", image: "img/wayne-barnett-founder-ceo.jpg" },
-    { name: "Angela Caroll", role: "Chief Editor", image: "img/angela-caroll-chief-editor.jpg" },
-    { name: "Walter Gordon", role: "Office Manager", image: "img/walter-gordon-office-manager.jpg" },
-    { name: "Angela Lopez", role: "Social Media Manager", image: "img/angela-lopez-social-media-manager.jpg" },
-    { name: "Scott Estrada", role: "Developer", image: "img/scott-estrada-developer.jpg" },
-    { name: "Barbara Ramos", role: "Graphic Designer", image: "img/barbara-ramos-graphic-designer.jpg" },
+    { name: "Wayne Barnett", role: "Founder & CEO", image: "wayne-barnett-founder-ceo.jpg" },
+    { name: "Angela Caroll", role: "Chief Editor", image: "angela-caroll-chief-editor.jpg" },
+    { name: "Walter Gordon", role: "Office Manager", image: "walter-gordon-office-manager.jpg" },
+    { name: "Angela Lopez", role: "Social Media Manager", image: "angela-lopez-social-media-manager.jpg" },
+    { name: "Scott Estrada", role: "Developer", image: "scott-estrada-developer.jpg" },
+    { name: "Barbara Ramos", role: "Graphic Designer", image: "barbara-ramos-graphic-designer.jpg" },
 ];
 
 console.table(team)
 
-wayneElement.innerHTML = `
-<img src="${team[0].image}" alt="${team[0].name}">
-<h3 class="mt-20">${team[0].name}</h3>
-<p class="mt-10">${team[0].role}</p>
-`
+// CREO UNA VARIABILE DI APPOGGIO PER POI STAMPARE LA CARD IN PAGINA
+let cards = "";
 
-angelaElement.innerHTML = `
-<img src="${team[1].image}" alt="${team[1].name}">
-<h3 class="mt-20">${team[1].name}</h3>
-<p class="mt-10">${team[1].role}</p>
-`
+// USO UN CICLO
+for (let member of team) {
+    // MONTO LA CARD E METTO DEGLI ELEMENTI DINAMICI
+    cards += `
+    <div class="card">
+    <img src="img/${member.image}" alt="${member.name}">
+    <h3 class="mt-20">${member.name}</h3>
+    <p class="mt-10">${member.role}</p>                  
+    </div>
+    `
+}
 
-walterElement.innerHTML = `
-<img src="${team[2].image}" alt="${team[2].name}">
-<h3 class="mt-20">${team[2].name}</h3>
-<p class="mt-10">${team[2].role}</p>
-`
-
-lopezElement.innerHTML = `
-<img src="${team[3].image}" alt="${team[3].name}">
-<h3 class="mt-20">${team[3].name}</h3>
-<p class="mt-10">${team[3].role}</p>
-`
-
-scottElement.innerHTML = `
-<img src="${team[4].image}" alt="${team[4].name}">
-<h3 class="mt-20">${team[4].name}</h3>
-<p class="mt-10">${team[4].role}</p>
-`
-
-barbaraElement.innerHTML = `
-<img src="${team[5].image}" alt="${team[5].name}">
-<h3 class="mt-20">${team[5].name}</h3>
-<p class="mt-10">${team[5].role}</p>
-`
-
-
-
-
-
-
-
+// STAMPO IN PAGINA
+cardContainer.innerHTML = cards;
 
 
 
